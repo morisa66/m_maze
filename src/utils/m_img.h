f67abafc6f50cf6ce0ffa212aab11a1237c1ac97
morisa66 <49175869+morisa66@users.noreply.github.com>
2021-03-15 14:37:59 +0800

    Update m_img.h

--- src/utils/m_img.h
+++ src/utils/m_img.h
@@ -30,7 +30,7 @@ unsigned m_load_img2D(const char* path)
 			format = GL_RGBA;
 
 		glBindTexture(GL_TEXTURE_2D, texture);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT); 
