c4d53063d0d4acdd313cac8c68f4ef2adb1b580b
morisa66 <49175869+morisa66@users.noreply.github.com>
2021-03-19 15:11:09 +0800

    Update m_define.h

--- src/utils/m_define.h
+++ src/utils/m_define.h
@@ -7,18 +7,18 @@ MORISA_NAMESPACE_BEGIN
 
 typedef  unsigned int shape_choice;
 
-#define SHAPE_CUBE						0x01
-#define SHAPE_QUAD						0x02
-#define SHAPE_PLANE						0x03
+#define SHAPE_CUBE						        0x01
+#define SHAPE_QUAD						        0x02
+#define SHAPE_PLANE						        0x03
 
 // vertex attibute
 typedef  unsigned int vertex_attribute;
 
-#define	VA_POSITION						0x01
-#define	VA_NORMAL						0x02
-#define	VA_POSITION_NORMAL				0x03
-#define	VA_TEXCOORD						0x04 
-#define	VA_POSITION_TEXCOORD			0x05
+#define	VA_POSITION						        0x01
+#define	VA_NORMAL						          0x02
+#define	VA_POSITION_NORMAL			      0x03
+#define	VA_TEXCOORD						        0x04 
+#define	VA_POSITION_TEXCOORD			    0x05
 #define	VA_POSITION_NORMAL_TEXCOORD 	0x07
 
 
